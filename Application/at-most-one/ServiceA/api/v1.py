8df7f85f4ef143fbd491b357f95a1fdfd562fee2
Шабалкин Александр Владимирович <a.shabalkin@tinkoff.ru>
2026-02-17 19:46:35 +0300

    f

--- Application/at-most-one/ServiceA/api/v1.py
+++ Application/at-most-one/ServiceA/api/v1.py
@@ -20,18 +20,9 @@ async def accept_and_forward(payload: Message):
     logger.info('send request to service-b')
 
     async with httpx.AsyncClient(timeout=2.0) as client:
-        try:
-            resp = await client.post(
-                f"{config.SERVICE_B_URL}/api/message-b",
-                json=payload.model_dump(),
-            )
-            resp.raise_for_status()
-        except Exception as e:
-            raise HTTPException(
-                status_code=502,
-                detail=f"Failed to forward to ServiceB: {type(e).__name__}",
-            )
-
-    logger.info("got response from service-b: %s", resp.text)
+        await client.post(
+            f"{config.SERVICE_B_URL}/api/message-b",
+            json=payload.model_dump(),
+        )
 
     return {"result": "ok"}
